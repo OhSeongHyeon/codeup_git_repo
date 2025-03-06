6d9fda3c2fe752db439cfb02971d45dac4ad363d
OhSeongHyeon <wert3976@gmail.com>
2025-03-06 16:47:21 +0900

    codeup python 6011~6020 압축풀이

---
+++ python_workspace/py6011-6020.py
@@ -0,0 +1,45 @@
+print("=== codeup python 6011 ===")
+# print(float(input()))
+
+print("=== codeup python 6012 ===")
+# a = input()
+# b = input()
+# print(int(a))
+# print(int(b))
+
+print("=== codeup python 6013 ===")
+# a = input()
+# b = input()
+# print(b)
+# print(a)
+
+print("=== codeup python 6014 ===")
+# a = input()
+# print(float(a))
+# print(float(a))
+# print(float(a))
+
+print("=== codeup python 6015 ===")
+# a, b = input().split()
+# print(int(a))
+# print(int(b))
+
+print("=== codeup python 6016 ===")
+# a, b = input().split()
+# print(b, a)
+
+print("=== codeup python 6017 ===")
+# s = input()
+# print(s, s, s)
+
+print("=== codeup python 6018 ===")
+# a, b = input().split(":")
+# print(a, b, sep = ":")
+
+print("=== codeup python 6019 ===")
+# y, m, d = input().split(".")
+# print(d, m, y, sep = "-")
+
+print("=== codeup python 6020 ===")
+s1, s2 = input().split("-")
+print("%s%s" % (s1, s2))
