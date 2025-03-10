a6f5fe4ff1ac7b98bb953e8dd4e593255d9ff15e
OhSeongHyeon <wert3976@gmail.com>
2025-03-10 10:26:12 +0900

    codeup python 6055~6060 and add file

--- python_workspace/py6051-6060.py
+++ python_workspace/py6051-6060.py
@@ -17,16 +17,20 @@ print("=== codeup python 6055 ===")
 # print(bool(int(a)) or bool(int(b)))
 
 print("=== codeup python 6056 ===")
-
+# list = [int(i) for i in input().split()]
+# print(list[0] != list[1])
 
 print("=== codeup python 6057 ===")
-
+# list = [int(i) for i in input().split()]
+# print(list[0] == list[1])
 
 print("=== codeup python 6058 ===")
-
+# list = [int(i) for i in input().split()]
+# print((list[0] == 0) and (list[1] == 0))
 
 print("=== codeup python 6059 ===")
-
+# print(~(int(input())))
 
 print("=== codeup python 6060 ===")
-
+list = [int(i) for i in input().split()]
+print(list[0] & list[1])
---
+++ python_workspace/py6061-6070.py
@@ -0,0 +1,29 @@
+print("=== codeup python 6061 ===")
+
+
+print("=== codeup python 6062 ===")
+
+
+print("=== codeup python 6063 ===")
+
+
+print("=== codeup python 6064 ===")
+
+
+print("=== codeup python 6065 ===")
+
+
+print("=== codeup python 6066 ===")
+
+
+print("=== codeup python 6067 ===")
+
+
+print("=== codeup python 6068 ===")
+
+
+print("=== codeup python 6069 ===")
+
+
+print("=== codeup python 6070 ===")
+
