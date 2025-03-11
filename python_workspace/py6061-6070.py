e7c0d62d1d9c66d3f36bf3808579ea10bd25dd82
OhSeongHyeon <wert3976@gmail.com>
2025-03-11 11:40:48 +0900

    codeup python 6061~6070

--- python_workspace/py6061-6070.py
+++ python_workspace/py6061-6070.py
@@ -1,29 +1,66 @@
 print("=== codeup python 6061 ===")
-
+# list = [int(i) for i in input().split()]
+# print(list[0] | list[1])
 
 print("=== codeup python 6062 ===")
-
+# list = [int(i) for i in input().split()]
+# print(list[0] ^ list[1])
 
 print("=== codeup python 6063 ===")
-
+# list = [int(i) for i in input().split()]
+# print(list[0] if(list[0] >= list[1]) else list[1])
 
 print("=== codeup python 6064 ===")
-
+# list = [int(i) for i in input().split()]
+# list.sort()
+# print(list[0])
 
 print("=== codeup python 6065 ===")
-
+# sp = input().split()
+# spl = len(sp)
+# sb = []
+# for i in range(spl):
+#     if(int(sp[i])%2 == 0):
+#         sb.append(sp[i])
+#         sb.append("\n" if i < spl - 1 else "")
+# print("".join(sb))
 
 print("=== codeup python 6066 ===")
-
+# g_sArr = input().split()
+# g_sArrLen = len(g_sArr)
+# sb = []
+# for i in range(g_sArrLen):
+#     sb.append("even" if int(g_sArr[i])%2 == 0 else "odd")
+#     sb.append("\n" if i < g_sArrLen - 1 else "")
+# print("".join(sb))
 
 print("=== codeup python 6067 ===")
-
+# n = int(input())
+# if n < 0:
+#     if n%2 == 0: print('A')
+#     else: print('B')
+# else:
+#     if n%2 == 0: print('C')
+#     else: print('D')
 
 print("=== codeup python 6068 ===")
-
+# n = int(input())
+# if(n >= 90): print('A')
+# elif(n >= 70): print('B')
+# elif(n >= 40): print('C')
+# else: print('D')
 
 print("=== codeup python 6069 ===")
-
+# c = input()
+# if(c == 'A'): print("best!!!")
+# elif(c == 'B'): print("good!!")
+# elif(c == 'C'): print("run!")
+# elif(c == 'D'): print("slowly~")
+# else: print("what?")
 
 print("=== codeup python 6070 ===")
-
+m = int(input())
+if(m//3 == 1): print("spring")
+elif(m//3 == 2): print("summer")
+elif(m//3 == 3): print("fall")
+else: print("winter")
