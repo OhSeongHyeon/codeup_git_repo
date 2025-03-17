9ea9404fe71911997d5c74d63411d5324e8ed568
OhSeongHyeon <wert3976@gmail.com>
2025-03-17 13:48:13 +0900

    codeup c 1021~1030

--- clang_workspace/c1021-1030.c
+++ clang_workspace/c1021-1030.c
@@ -28,8 +28,21 @@ int main()
     return 0;
 } */
 
+/* #include <stdio.h>
+
+int main()
+{
+    char wds[31];
+    scanf("%s", wds);
+    for(int i = 0; wds[i] != '\0'; i++)
+    {
+        printf("\'%c\'\n", wds[i]);
+    }
+    return 0;
+} */
+
 //1025 1개의 숫자로 각각 구분되어 저장
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main()
 {
@@ -41,7 +54,54 @@ int main()
     printf("[%d]\n", d*10);
     printf("[%d]\n", e*1);
     return 0;
+} */
+
+/* #include <stdio.h>
+
+int main()
+{
+    int h, m, s;
+    scanf("%d:%d:%d", &h, &m, &s);
+    printf("%d", m);
+    return 0;
+} */
+
+/* #include <stdio.h>
+
+int main()
+{
+    int y, m, d;
+    scanf("%d.%d.%d", &y, &m, &d);
+    printf("%02d-%02d-%04d", d, m, y);
+    return 0;
+} */
+
+/* #include <stdio.h>
+
+int main() {
+    unsigned int n;
+    scanf("%u", &n);
+    printf("%u", n);
+    return 0;
+} */
+
+/* #include <stdio.h>
+
+int main()
+{
+    double d;
+    scanf("%lf", &d);
+    printf("%.11lf", d);
+    return 0;
+} */
+
+#include <stdio.h>
+
+int main()
+{
+    long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+    return 0;
 }
-
-
 
---
+++ clang_workspace/c1031-1040.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    //1031
+    // int dto;
+    // scanf("%d", &dto);
+    // printf("%o", dto);
+
+    //1032
+
+    return 0;
+}
