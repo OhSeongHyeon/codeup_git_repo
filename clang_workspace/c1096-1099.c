eb47b3bc4227ec12833c56e270dfe4241a47c2a0
OhSeongHyeon <wert3976@gmail.com>
2025-03-21 16:45:26 +0900

    codeup c 1096

---
+++ clang_workspace/c1096-1099.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    //1096
+    // int n;
+    // scanf("%d", &n);
+    // int arr[19][19] = {};
+    // int i, j;
+    // for (i = 0; i < n; i++) {
+    //     int x, y;
+    //     scanf("%d %d", &x, &y);
+    //     arr[x-1][y-1] = 1;
+    // }
+    // for (i = 0; i < 19; i++) {
+    //     for (j = 0; j < 19; j++) {
+    //         printf("%d ", arr[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    //1097
+    
+
+    return 0;
+}
